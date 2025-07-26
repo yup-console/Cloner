ec1b3f2b4cbc86e62ee35bf74ae7661d40549db1
Console <138242046+yup-console@users.noreply.github.com>
2025-07-26 15:35:38 +0530

    Add files via upload

---
+++ utils/innit.py
@@ -0,0 +1,38 @@
+
+import requests
+import threading
+import asyncio
+import base64
+from pystyle import *
+import random
+from datetime import datetime
+import os
+import time
+
+def logo():
+    cloner_art = """
+ ▄████▄   ██▓     ▒█████   ███▄    █ ▓█████  ██▀███  
+▒██▀ ▀█  ▓██▒    ▒██▒  ██▒ ██ ▀█   █ ▓█   ▀ ▓██ ▒ ██▒
+▒▓█    ▄ ▒██░    ▒██░  ██▒▓██  ▀█ ██▒▒███   ▓██ ░▄█ ▒
+▒▓▓▄ ▄██▒▒██░    ▒██   ██░▓██▒  ▐▌██▒▒▓█  ▄ ▒██▀▀█▄  
+▒ ▓███▀ ░░██████▒░ ████▓▒░▒██░   ▓██░░▒████▒░██▓ ▒██▒
+░ ░▒ ▒  ░░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░
+  ░  ▒   ░ ░ ▒  ░  ░ ▒ ▒░ ░ ░░   ░ ▒░ ░ ░  ░  ░▒ ░ ▒░
+░          ░ ░   ░ ░ ░ ▒     ░   ░ ░    ░     ░░   ░ 
+░ ░          ░  ░    ░ ░           ░    ░  ░   ░     
+░
+"""
+    centered_art = "\n".join(" " * 15 + line for line in cloner_art.strip("\n").splitlines())
+    print(Colorate.Horizontal(Colors.red_to_yellow, centered_art))
+    
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def current_time():
+    return datetime.now().strftime("%H:%M:%S")
+
+b = Colors.dark_blue
+r = Colors.red
+g = Colors.green
+y = Colors.yellow
+w = Colors.white
