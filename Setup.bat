5b7fad81a863e7a402a7a395db7e64409b881596
Console <138242046+yup-console@users.noreply.github.com>
2025-07-26 13:55:56 +0530

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,16 @@
+@echo off
+pip install psutil
+pip install Presence
+pip install discord
+pip install asyncio
+pip install colorama
+pip install fore
+pip install pystyle
+pip3 install --user psutil
+pip3 install --user pypresence
+pip3 install --user discord
+pip3 install --user asyncio
+pip3 install --user colorama
+pip3 install --user platforms
+pip3 install discord==1.7.3
+pip3 install discord.py==1.7.3
